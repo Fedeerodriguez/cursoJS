78dcb24b4ba56595c86c19808b1b23c494420ddd
Federico Rodriguez <fedeerodriguez1703@gmail.com>
2023-10-02 23:50:04 -0300

    Finalizando pre entrega 3

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 // El array de objetos (Los productos)
 let productos = [
-    { id: 4, nombre: "Guantes de boxeo", categoria: "boxeo", stock: 5, precio: 1500, rutaImg: "guantes-de-boxeo.jpg" },
-    { id: 6, nombre: "Pelota de futbol", categoria: "futbol", stock: 9, precio: 1900, rutaImg: "pelota-de-futbol.jpg" },
-    { id: 8, nombre: "Guantes de beisbol", categoria: "beisbol", stock: 4, precio: 700, rutaImg: "guante-de-beisbol.jpg" },
-    { id: 12, nombre: "Guantes de golero", categoria: "futbol", stock: 7, precio: 1400, rutaImg: "guantes-de-golero.jpg" },
-    { id: 10, nombre: "Raqueta de tenis", categoria: "tenis", stock: 2, precio: 3100, rutaImg: "raqueta-de-tenis.jpg" },
-    { id: 2, nombre: "Gorra de beisbol", categoria: "beisbol", stock: 12, precio: 800, rutaImg: "gorra-de-beisbol.jpg" },
-    { id: 9, nombre: "Pelota de tenis", categoria: "tenis", stock: 5, precio: 2200, rutaImg: "pelota-de-tenis.jpg" },
-    { id: 11, nombre: "Bolso de tenis", categoria: "tenis", stock: 8, precio: 990, rutaImg: "bolso-de-tenis.jpg" },
-    { id: 17, nombre: "Medias de futbol", categoria: "futbol", stock: 3, precio: 850, rutaImg: "medias-de-futbol.jpg" },
-    { id: 15, nombre: "Bate de beisbol", categoria: "beisbol", stock: 7, precio: 2300, rutaImg: "bate-de-beisbol.jpg" },
-    { id: 7, nombre: "Bucal de boxeo", categoria: "boxeo", stock: 10, precio: 1100, rutaImg: "bucal-de-boxeo.jpg" },
-    { id: 13, nombre: "Bolsa de boxeo", categoria: "boxeo", stock: 6, precio: 2750, rutaImg: "bolsa-de-boxeo.jpg" }
+    { id: 4, nombre: "guantes de boxeo", categoria: "boxeo", stock: 5, precio: 1500, rutaImg: "guantes-de-boxeo.jpg" },
+    { id: 6, nombre: "pelota de futbol", categoria: "futbol", stock: 9, precio: 1900, rutaImg: "pelota-de-futbol.jpg" },
+    { id: 8, nombre: "guantes de beisbol", categoria: "beisbol", stock: 4, precio: 700, rutaImg: "guante-de-beisbol.jpg" },
+    { id: 12, nombre: "guantes de golero", categoria: "futbol", stock: 7, precio: 1400, rutaImg: "guantes-de-golero.jpg" },
+    { id: 10, nombre: "raqueta de tenis", categoria: "tenis", stock: 2, precio: 3100, rutaImg: "raqueta-de-tenis.jpg" },
+    { id: 2, nombre: "gorra de beisbol", categoria: "beisbol", stock: 12, precio: 800, rutaImg: "gorra-de-beisbol.jpg" },
+    { id: 9, nombre: "pelota de tenis", categoria: "tenis", stock: 5, precio: 2200, rutaImg: "pelota-de-tenis.jpg" },
+    { id: 11, nombre: "bolso de tenis", categoria: "tenis", stock: 8, precio: 990, rutaImg: "bolso-de-tenis.jpg" },
+    { id: 17, nombre: "medias de futbol", categoria: "futbol", stock: 3, precio: 850, rutaImg: "medias-de-futbol.jpg" },
+    { id: 15, nombre: "bate de beisbol", categoria: "beisbol", stock: 7, precio: 2300, rutaImg: "bate-de-beisbol.jpg" },
+    { id: 7, nombre: "bucal de boxeo", categoria: "boxeo", stock: 10, precio: 1100, rutaImg: "bucal-de-boxeo.jpg" },
+    { id: 13, nombre: "bolsa de boxeo", categoria: "boxeo", stock: 6, precio: 2750, rutaImg: "bolsa-de-boxeo.jpg" }
 ]
 
 let carrito = []
@@ -48,7 +48,7 @@ function renderizarProductos(productos, carrito) {
 // funcion para agregar al carrito
 function agregarAlCarrito(productos, carrito, e) {
     let productoBuscado = productos.find(producto => producto.id === Number(e.target.id))
-    let productosEnCarrito = carrito.find(producto => producto.id === productoBuscado)
+    let productosEnCarrito = carrito.find(producto => producto.id === productoBuscado.id)
 
     if (productoBuscado.stock > 0) {
         if (productosEnCarrito) {
@@ -57,6 +57,7 @@ function agregarAlCarrito(productos, carrito, e) {
         } else {
             carrito.push({
                 id: productoBuscado.id,
+                rutaImg: productoBuscado.rutaImg,
                 nombre: productoBuscado.nombre,
                 precioUnitario: productoBuscado.precio,
                 unidades: 1,
@@ -65,8 +66,9 @@ function agregarAlCarrito(productos, carrito, e) {
         }
         productoBuscado.stock--
         localStorage.setItem("carrito", JSON.stringify(carrito))
+        totatada("Producto agregado", 1500)
     } else {
-        alert("No hay mas stock del producto seleccionado")
+        alertas("Lo siento", "No hay mas stock del producto deseado", "error", true)
     }
 
     renderizarCarrito(carrito)
@@ -83,7 +85,7 @@ if(productosEnElCarrito.length > 0){
         let tarjetaEnCarrito = document.createElement("div")
         tarjetaEnCarrito.className = "tarjeta"
         tarjetaEnCarrito.innerHTML = `
-             
+             <img class="imagenes" src=./imagenes/${producto.rutaImg}>
              <h3>${producto.nombre}</h3>
              <p>Precio: $${producto.precioUnitario}</p>
              <p> Cantidad: ${producto.unidades}</p>
@@ -96,6 +98,7 @@ if(productosEnElCarrito.length > 0){
     })
     let boton = document.createElement("button")
     boton.innerHTML = "Finalizar compra"
+    boton.className = "botonfinalizar"
     boton.addEventListener("click", ()=> finalizarCompra(carrito))
     divCarrito.appendChild(boton)
 }
@@ -145,13 +148,34 @@ function filtrarCategorias(e, productos) {
 
  function finalizarCompra() {
     let carrito = document.getElementById("carrito")
-    carrito.innerHTML = ""
+    carrito.innerHTML = "Ingrese productos para finalizar su compra"
     localStorage.removeItem("carrito")
-    
+    alertas("Gracias por su compra", "Su compra fue realizada con exito", "success", true)
     
 } 
+
+function alertas(title, text, icon, showconfirmButton, timer) {
+    Swal.fire({
+        title,
+        text,
+        icon,
+        showconfirmButton,
+        timer,
+    })
+}
+
+function totatada(text, duration) {
+    Toastify({
+        text,
+        duration,
+        className: "tostada",
+        style: {
+            background: "linear-gradient(to right, #2fb4cc, #0000ff)",
+          }
+        }).showToast()
+}
    
- //let total = carrito.reduce((acum, produto) => acum + produto.subtotal, 0)    
+//let total = carrito.reduce((acum, produto) => acum + produto.subtotal, 0)  
        
 
 
